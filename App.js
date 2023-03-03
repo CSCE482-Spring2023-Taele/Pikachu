adb8261bea6cf32be79efd1818a1e2e5ae907357
isafernandez <16018+isafernandez@users.noreply.github.tamu.edu>
2023-03-03 11:41:32 -0600

    update base frontend

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from './Home.js';
+
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Pave in Progress</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={"Home"} screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Settings" component={SettingsScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
